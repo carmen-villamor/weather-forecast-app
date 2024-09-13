9c2a1229d3fbb5d19ddb518eafb53f1a30805626
carmen-villamor <maicavillamor@gmail.com>
2024-09-13 20:36:10 +1000

    Added search functionality with API

--- src/app.js
+++ src/app.js
@@ -1,9 +1,29 @@
+function updateWeather(response) {
+  //console.log(response.data);
+  let temperature = response.data.temperature.current;
+  let city = response.data.city;
+  //console.log(temperature);
+  let tempValueElement = document.querySelector("#weather-app-temp-value");
+  let cityElement = document.querySelector("#weather-app-city");
+  tempValueElement.innerHTML = Math.round(temperature);
+  cityElement.innerHTML = city;
+}
+
+function searchCity(city) {
+  let apiKey = "b31e4f8fab9a29e19b02bcteb776oab4";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function handleSubmit(event) {
   event.preventDefault();
   let input = document.querySelector("#search-form-input");
-  let city = document.querySelector("#weather-app-city");
-  city.innerHTML = input.value;
+  //let city = document.querySelector("#weather-app-city");
+  //city.innerHTML = input.value;
+  searchCity(input.value);
 }
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
+
+searchCity("sydney");
